49014ab9366d7c49ab096e76a44402948e36ae25
Sam Oates <sam@samoatesgames.com>
2019-08-17 23:16:06 +0100

    Use the workspace to check how much space is free.
    Convert to using Mb rather than Gb for people who require smaller values.
    If out of space, mark the machine offline so we don't keep failing jobs.

--- src/main/java/org/jenkinsci/plugin/Diskcheck.java
+++ src/main/java/org/jenkinsci/plugin/Diskcheck.java
@@ -2,106 +2,93 @@ package org.jenkinsci.plugin;
 
 import hudson.AbortException;
 import hudson.Extension;
+import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.BuildListener;
 import hudson.model.AbstractBuild;
-import hudson.model.Descriptor;
 import hudson.model.Computer;
+import hudson.model.Descriptor;
 import hudson.tasks.BuildWrapper;
-import hudson.node_monitors.*;
 import hudson.model.Node;
-
+import hudson.slaves.OfflineCause;
 import org.kohsuke.stapler.DataBoundConstructor;
-
 import java.io.IOException;
 import java.io.PrintStream;
-
-import hudson.node_monitors.DiskSpaceMonitorDescriptor;
+import jenkins.model.Jenkins;
+import org.jvnet.localizer.Localizable;
 
 public class Diskcheck extends BuildWrapper {
 
-	public final boolean failOnError;
+    @DataBoundConstructor
+    public Diskcheck() {
+    }
 
-	@DataBoundConstructor
-	public Diskcheck(boolean failOnError) {
-		this.failOnError = failOnError;
-	}
-	
-	@Override
-	public Environment setUp(AbstractBuild build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException {
-		return new NoopEnv();
-	}
+    @Override
+    public Environment setUp(AbstractBuild build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException {
+        return new NoopEnv();
+    }
 
-	@Override
-	public void preCheckout(AbstractBuild build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException {
-		PrintStream log = listener.getLogger();
+    @Override
+    public void preCheckout(AbstractBuild build, Launcher launcher, BuildListener listener) throws IOException, InterruptedException {
+        PrintStream log = listener.getLogger();
 
-		int spaceThreshold = PluginImpl.getInstance().getSpacecheck();
+        FilePath workspace = build.getWorkspace();
+        if (workspace == null) {
+            return; // No valid workspace, no way for us to validate.
+        }
+        
+        Node node = build.getBuiltOn();
+        if (node == null) {
+            return; // No valid node, no way for us to validate.
+        }
+        
+        int requiredSpaceInMb = PluginImpl.getInstance().getRequiredSpaceInMb();
 
-		log.println("[DiskCheck] Disk space threshold is set to: " + spaceThreshold + "Gb");
-		log.println("[DiskCheck] Checking disk space now...");
+        log.println("[DiskCheck] Disk space threshold is set to: " + requiredSpaceInMb + "Mb");
+        log.println("[DiskCheck] Checking disk space...");
 
-		// Ensure the workspace exists.
-		if (!build.getWorkspace().exists()) {
-			build.getWorkspace().mkdirs();
-		}
+        // Ensure the workspace exists.
+        if (!workspace.exists()) {
+            workspace.mkdirs();
+        }
+        
+        String nodeName = node.getNodeName();
+        if (nodeName.equals("")) {
+            nodeName = "master";
+        }
 
-		Node node = build.getBuiltOn();
-		Computer computer = node.toComputer();
-		String nodeName = build.getBuiltOnStr();
-		if (nodeName == null || nodeName == "") {
-			nodeName = "master";
-		}
-		
-		int roundedFreeSpaceInGb = 0;
-		try 
-		{
-			if (computer != null) {
+        long roundedFreeSpaceInBytes = workspace.getUsableDiskSpace();
+        long roundedFreeSpaceInKb = roundedFreeSpaceInBytes / (long)1024;
+        long roundedFreeSpaceInMb = roundedFreeSpaceInKb / (long)1024;
+        
+        log.println("[DiskCheck] Total disk space in " + nodeName + "'s workspace is: " + roundedFreeSpaceInMb + "Mb");
 
-				if (DiskSpaceMonitor.DESCRIPTOR.get(computer) == null) {
-					log.println("[DiskCheck] No slave data available trying to get data from slave");
-					Thread.sleep(1000);
+        if (roundedFreeSpaceInMb < requiredSpaceInMb) {
+            Computer computer = node.toComputer();
+            if (computer != null) {
+                Localizable message = hudson.slaves.Messages._SlaveComputer_DisconnectedBy(
+                        "[DiskCheck Plugin]", 
+                        " : Disk Space is less than " + requiredSpaceInMb + "Mb"
+                );
+                computer.setTemporarilyOffline(true, OfflineCause.create(message));
+            }
+            throw new AbortException("[DiskCheck] Disk Space is too low please look into it before starting a build");
+        }
+    }
 
-					if (DiskSpaceMonitor.DESCRIPTOR.get(computer) == null) {
-						log.println("[DiskCheck] Could not get slave information, exiting disk check for this slave");
-						return;
-					}
-				}
+    @Extension
+    public static final class DescriptorImpl extends Descriptor<BuildWrapper> {
 
-				AbstractDiskSpaceMonitor.getAll();
+        @Override
+        public String getDisplayName() {
+            return "Enable disk check";
+        }
 
-				long size = DiskSpaceMonitor.DESCRIPTOR.get(computer).size;
-				String baseName = build.getWorkspace().getBaseName();
-				
-				DiskSpaceMonitorDescriptor.DiskSpace diskSpaceMonitor = new DiskSpaceMonitorDescriptor.DiskSpace(baseName, size);
-				String diskSpace = diskSpaceMonitor.getGbLeft();
-				roundedFreeSpaceInGb = (int) (Double.parseDouble(diskSpace));
-			}
-		}
-		catch (NullPointerException e){
-			log.println("Could not get slave disk size information, exiting disk check for this slave");
-			return;
-		}
-		
-		log.println("Total disk space in " + nodeName + "'s workspace is: " + roundedFreeSpaceInGb + "Gb");
-		
-		if (roundedFreeSpaceInGb < spaceThreshold) {
-			throw new AbortException("Disk Space is too low please look into it before starting a build");
-		}
-	}
+        public DescriptorImpl() {
+            load();
+        }
+    }
 
-
-	@Extension
-	public static final class DescriptorImpl extends Descriptor<BuildWrapper> {
-		public String getDisplayName() {
-			return "Enable Disk Check";
-		}
-
-		public DescriptorImpl() {
-			load();
-		}
-	}
-
-	class NoopEnv extends Environment {
-	}
+    class NoopEnv extends Environment {
+    }
 }
--- src/main/java/org/jenkinsci/plugin/PluginImpl.java
+++ src/main/java/org/jenkinsci/plugin/PluginImpl.java
@@ -9,26 +9,27 @@ import net.sf.json.JSONObject;
 import org.kohsuke.stapler.StaplerRequest;
 
 public class PluginImpl extends Plugin {
-	private int spacecheck = 2;
-	
+
+    private int m_requiredSpaceInMb = 2048;
+
     public static PluginImpl getInstance() {
         return Jenkins.getInstance().getPlugin(PluginImpl.class);
     }
-	
-	@Override
-	public void configure(StaplerRequest req, JSONObject formData) throws IOException, ServletException, FormException {
-		formData = formData.getJSONObject("disk-check");
-		spacecheck = formData.getInt("spacecheck");
-		
-		save();
-		super.configure(req, formData);
-	}
-		
-	public int getSpacecheck() {
-		return spacecheck;
-	}
-	
-	public void setSpacecheck(int spaceheck) {
-		this.spacecheck = spacecheck;
-	}
+
+    @Override
+    public void configure(StaplerRequest req, JSONObject formData) throws IOException, ServletException, FormException {
+        formData = formData.getJSONObject("disk-check");
+        m_requiredSpaceInMb = formData.getInt("requiredSpaceInMb");
+
+        save();
+        super.configure(req, formData);
+    }
+
+    public int getRequiredSpaceInMb() {
+        return m_requiredSpaceInMb;
+    }
+
+    public void setRequiredSpaceInMb(int requiredSpaceInMb) {
+        m_requiredSpaceInMb = requiredSpaceInMb;
+    }
 }
